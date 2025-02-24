4fbdda0b343456b807dbf26df4c6d2b351d1fccc
Nick Stout <stout.nicholas2@gmail.com>
2025-02-24 17:09:48 +0000

    Added Boilerplate MessageDAO

--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -5,5 +5,27 @@ import java.sql.*;
 import java.util.*;
 
 public class MessageDAO {
-    
+    public Message insertMessage(Message message) {
+        return null;
+    }
+
+    public List<Message> retrieveAllMessages() {
+        return null;
+    }
+
+    public Message retrieveMessageById(int message_id) {
+        return null;
+    }
+
+    public void deleteMessageById(int message_id) {
+        
+    }
+
+    public Message updateMessageById(int message_id) {
+        return null;
+    }
+
+    public List<Message> retrieveAllMessagesByUser(int user_id) {
+        return null;
+    }
 }
