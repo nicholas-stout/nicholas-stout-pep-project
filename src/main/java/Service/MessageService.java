e3e16be7d71a7678630492ee538495bbc0a7edcc
Nick Stout <stout.nicholas2@gmail.com>
2025-02-26 17:07:17 +0000

    Finished MessageService

--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -26,66 +26,84 @@ public class MessageService {
     }
 
     /**
-     * TODO add logic
      * This method will attempt to persist a message to the database. It will first verify if the message is valid.
      * @param message the message we wish to add
      * @return the persisted message, if successful, null otherwise
      */
     public Message addMessage(Message message) {
-        
-        return null;
+        // Validate message
+        if (isValidMessage(message)) {
+            return null;
+        }
+
+        return messageDAO.insertMessage(message);
     }
 
     /**
-     * TODO Add logic
      * This method will simply return all messages in our database
      * @return a list of every message is the database. The list will be empty if no messages have been added
      */
     public List<Message> getAllMessages() {
-        return null;
+        return messageDAO.retrieveAllMessages();
     }
 
     /**
-     * TODO Add logic
      * This method will return a message based on its message_id
      * @param message_id The message_id of the message we wish to retrieve
      * @return The message if it exists, null otherwise
      */
     public Message getMessageById(int message_id) {
-        return null;
+        return messageDAO.retrieveMessageById(message_id);
     }
 
     /**
-     * TODO Add logic
      * This method will delete a message based on its message_id, and will return it.
      * @param message_id The id of the message we wish to delete
      * @return The deleted message if it exists, null otherwise
      */
     public Message deleteMessageById(int message_id) {
-        return null;
+        // Create Message object that will contain the data of the message we wish to delete. It will be null if it does not exist.
+        Message message = messageDAO.retrieveMessageById(message_id);
+        
+        // If the message does not exist, just return null. Only try to delete the message if we're sure it exists.
+        if (message == null) {
+            return null;
+        } 
+            
+        messageDAO.deleteMessageById(message_id);
+        return message;
     }
 
     /**
-     * TODO Add logic
      * This method will update a message's text based on its message_id
      * @param message_id The id of the message we wish to update
      * @return The updated message if the original message exists and the new message is valid, null otherwise
      */
-    public Message updateMessageById(int message_id) {
-        return null;
+    public Message updateMessageById(int message_id, String newMessageText) {
+        // Check if original message exists
+        if (messageDAO.retrieveMessageById(message_id) == null) {
+            return null;
+        }
+
+        // Check if new message text is valid
+        if (isValidMessageText(newMessageText)) {
+            return null;
+        }
+        
+        // update the message
+        messageDAO.updateMessageById(message_id, newMessageText);
+        return messageDAO.retrieveMessageById(message_id);
     }
 
     /**
-     * TODO Add logic
      * @param posted_by The id of the user whose messages we wish to query
      * @return A list of all messages posted by this user 
      */
     public List<Message> getAllMessagesByUser(int posted_by) {
-        return null;
+        return messageDAO.retrieveAllMessagesByUser(posted_by);
     }
 
     /**
-     * TODO Add logic
      * This method will check if a Message object represents a valid message.
      * A message is valid if it is posted by a real user and its text is not empty and not more than 255 characters.
      * @param message The message we are validating
