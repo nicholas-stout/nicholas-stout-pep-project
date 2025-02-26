bdb776b2342c4d2adb4a754f420192b8acc4ce00
Nick Stout <stout.nicholas2@gmail.com>
2025-02-26 15:22:00 +0000

    Updated DAO Classes

--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -1,6 +1,8 @@
 package DAO;
 import Model.Message;
 import Util.ConnectionUtil;
+
+import java.lang.Thread.State;
 import java.sql.*;
 import java.util.*;
 
@@ -16,14 +18,20 @@ public class MessageDAO {
         
         try {
             String sql = "INSERT INTO message(posted_by, message_text, time_posted_epoch) VALUES (?, ?, ?);";
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
             preparedStatement.setInt(1, message.getPosted_by());
             preparedStatement.setString(2, message.getMessage_text());
             preparedStatement.setLong(3, message.getTime_posted_epoch());
 
             preparedStatement.executeUpdate();
-            return message;
+            
+            ResultSet pkeyResultSet = preparedStatement.getGeneratedKeys();
+
+            if (pkeyResultSet.next()) {
+                int generated_message_id = pkeyResultSet.getInt(1);
+                return new Message(generated_message_id, message.getPosted_by(), message.getMessage_text(), message.getTime_posted_epoch());
+            }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
