befcd8d25b4e57c022aca65183cec111c16beb3b
Nick Stout <stout.nicholas2@gmail.com>
2025-03-04 19:26:56 +0000

    Added Method Stubs to SocialMediaController.java

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -16,7 +16,15 @@ public class SocialMediaController {
      */
     public Javalin startAPI() {
         Javalin app = Javalin.create();
-        app.get("example-endpoint", this::exampleHandler);
+        app.get("example-endpoint", this::exampleHandler); // TODO delete line
+        app.post("/register", this::postRegisterHandler);
+        app.post("/login", this::postLoginHandler);
+        app.post("/messages", this::postMessagesHandler);
+        app.get("/messages", this::getMessagesHandler);
+        app.get("/messages/{message_id}", this::getMessageIdHandler);
+        app.delete("/messages/{message_id}", this::deleteMessageIdHandler);
+        app.patch("/messages/{message_id}", this::patchMessageIdHandler);
+        app.get("/accounts/{account_id}/messages", this::getAccountIdMessagesHandler);
 
         return app;
     }
@@ -29,5 +37,35 @@ public class SocialMediaController {
         context.json("sample text");
     }
 
+    private void postRegisterHandler(Context ctx) {
+        
+    }
 
+    private void postLoginHandler(Context ctx) {
+
+    }
+
+    private void postMessagesHandler(Context ctx) {
+
+    }
+
+    private void getMessagesHandler(Context ctx) {
+
+    }
+
+    private void getMessageIdHandler(Context ctx) {
+
+    }
+
+    private void deleteMessageIdHandler(Context ctx) {
+
+    }
+
+    private void patchMessageIdHandler(Context ctx) {
+
+    }
+
+    private void getAccountIdMessagesHandler(Context ctx) {
+
+    }
 }
