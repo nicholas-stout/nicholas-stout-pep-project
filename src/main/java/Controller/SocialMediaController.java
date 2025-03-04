abacd1eb9e9f040da0466287ee2f115f5df05c68
Nick Stout <stout.nicholas2@gmail.com>
2025-03-04 19:37:28 +0000

    Completed Handler for POST localhost:8080/register

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -3,12 +3,21 @@ package Controller;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import Model.*;
+import Service.*;
+
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller. The endpoints you will need can be
  * found in readme.md as well as the test cases. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
 public class SocialMediaController {
+    private AccountService accountService = new AccountService();
+    private MessageService messageService = new MessageService();
+    
     /**
      * In order for the test cases to work, you will need to write the endpoints in the startAPI() method, as the test
      * suite must receive a Javalin object from this method.
@@ -37,8 +46,22 @@ public class SocialMediaController {
         context.json("sample text");
     }
 
-    private void postRegisterHandler(Context ctx) {
-        
+    /**
+     * Handler for the endpoint POST localhost:8080/register. This handler will create a new Account and persist it to the database.
+     * @param ctx Context object for information about HTTP request and response
+     * @throws JsonProcessingException will be thrown if the JSON cannot be parsed
+     */
+    private void postRegisterHandler(Context ctx) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Account account = mapper.readValue(ctx.body(), Account.class);
+        Account addedAccount = accountService.addAccount(account);
+
+        // check if Account was successfully added
+        if (addedAccount != null) {
+            ctx.json(addedAccount);
+        } else {
+            ctx.status(400);
+        }
     }
 
     private void postLoginHandler(Context ctx) {
